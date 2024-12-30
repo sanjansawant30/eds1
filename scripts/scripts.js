60be7c1e94187857682e78e772275023956d4224
sanjansawant30 <sanjan.sawant30@gmail.com>
2024-12-30 15:27:44 +0530

    modal block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -53,6 +53,19 @@ function buildAutoBlocks(main) {
   }
 }
 
+
+function autolinkModals(element) {
+  element.addEventListener('click', async (e) => {
+    const origin = e.target.closest('a');
+
+    if (origin && origin.href && origin.href.includes('/modals/')) {
+      e.preventDefault();
+      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
+      openModal(origin.href);
+    }
+  });
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
